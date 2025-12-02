7e23018d69cf3f920ef6fff4fd05c0fbc902675c
catchsob <42635950+catchsob@users.noreply.github.com>
2025-12-02 09:51:27 +0800

    Add files via upload

---
+++ AI 應用開發與部署/cwaapi.py
@@ -0,0 +1,20 @@
+import flask
+import cwa
+
+with open('env.json', encoding='utf-8') as f:
+    env = json.load(f)
+
+app = Flask(__name__)
+
+cwa_key = env['key']
+
+@app.route("/cwa", methods=['GET', 'POST'])
+def cwa_():
+    '''http://127.0.0.1:5000/cwa?site=臺北''' 
+    site = flask.request.args.get('site')
+    if site:
+        return cwa.tostr(cwa.cwa(site, cwa_key), ', ')
+    return 'Bad Request', 400
+
+if __name__ == "__main__":
+    app.run()
